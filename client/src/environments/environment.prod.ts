b9e78455e17e793cd1be65e4ab9b2f5aa3ed6bab
Yasmany Santalla Pereda <ysantalla88@gmail.com>
2019-03-18 12:02:01 -0400

    updating prod env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  httpLinkServer: 'http://10.2.24.227:5000/graphql',
-  wsLinkServer: 'ws://10.2.24.227:5000/subscriptions',
-  downloadLinkServer: 'http://10.2.24.227:5000/download',
+  httpLinkServer: 'http://gespro.upr.edu.cu:5000/graphql',
+  wsLinkServer: 'ws://gespro.upr.edu.cu:5000/subscriptions',
+  downloadLinkServer: 'http://gespro.upr.edu.cu:5000/download',
   directoryLink: "http://directorio.upr.edu.cu/search",
   envName: 'Production',
   appName: 'GESPRO',
